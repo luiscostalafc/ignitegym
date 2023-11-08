18a4f6882f11f6498f49ea7cfc001c2a96634a27
Luis Costa <luiscostalafc@gmail.com>
2023-11-08 05:00:37 -0300

    feat: include NativeBase configuration

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
-import { Text, View, StatusBar } from "react-native";
+import { StatusBar } from "react-native";
+import { GluestackUIProvider, Text, Box } from "@gluestack-ui/themed";
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
+import { config } from "@gluestack-ui/config";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -12,24 +14,26 @@ export default function App() {
   });
 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#202024",
-      }}
-    >
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {fontsLoaded ? (
-        <Text style={{ color: "white" }}>Hello World!</Text>
-      ) : (
-        <Text>Loading...</Text>
-      )}
-    </View>
+    <GluestackUIProvider config={config}>
+      <Box
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "#202024",
+        }}
+      >
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? (
+          <Text style={{ color: "white" }}>Hello World!</Text>
+        ) : (
+          <Text>Loading...</Text>
+        )}
+      </Box>
+    </GluestackUIProvider>
   );
 }
