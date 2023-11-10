2efdbd0b51cae8d0d99e113eac08ffff48fb5484
Luis Costa <luiscostalafc@gmail.com>
2023-11-10 06:51:02 -0300

    fix: update in SignIn component

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,20 +1,30 @@
-import { VStack, Image } from "native-base";
+import { VStack, Image, Center, Text, Heading } from "native-base";
 
 import backgroundImg from "@assets/background.png";
 import LogoSvg from "@assets/logo.svg";
 
 export function SignIn() {
   return (
-    <VStack flex={1} bg="gray.700">
+    <VStack flex="1" bg="gray.700">
       <Image
         source={backgroundImg}
-        alt="Background"
-        resizeMode="cover"
+        alt="Pessoas treinando"
+        resizeMode="contain"
         position="absolute"
-        size="full"
       />
+      <Center my={24}>
+        <LogoSvg />
 
-      <LogoSvg />
+        <Text color="gray.100" fontSize="sm">
+          Treine o seu corpo e a sua mente
+        </Text>
+      </Center>
+
+      <Center>
+        <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+          Acesse sua conta
+        </Heading>
+      </Center>
     </VStack>
   );
 }
