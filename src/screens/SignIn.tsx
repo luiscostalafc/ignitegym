3892bc269d4f43d0f70be8398502a6cb4866e44e
Luis Costa <luiscostalafc@gmail.com>
2023-11-14 04:03:38 -0300

    feat: create Input component

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -2,10 +2,11 @@ import { VStack, Image, Center, Text, Heading } from "native-base";
 
 import backgroundImg from "@assets/background.png";
 import LogoSvg from "@assets/logo.svg";
+import { Input } from "@components/Input";
 
 export function SignIn() {
   return (
-    <VStack flex="1" bg="gray.700">
+    <VStack flex="1" bg="gray.700" px={10}>
       <Image
         source={backgroundImg}
         alt="Pessoas treinando"
@@ -24,6 +25,12 @@ export function SignIn() {
         <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
           Acesse sua conta
         </Heading>
+        <Input
+          placeholder="E-mail"
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        <Input placeholder="Senha" secureTextEntry />
       </Center>
     </VStack>
   );
